68b951344de53f7795088afa510653eadb03260e
QinLoong <2722937531@qq.com>
2023-06-25 20:36:46 +0800

    生成CRUD模版

--- plop-crud/index.js
+++ plop-crud/index.js
@@ -5,3 +5,12 @@ function bigCamelCase(fieldName) {
     const bigCamelCaseStringPices = stringPices.map((s) => s.substring(0, 1).toUpperCase() + s.substring(1));
     return bigCamelCaseStringPices.join('');
 }
+
+module.exports = {
+    description: '生成CRUD模版',
+    prompts: [
+      {
+        type: 'input',
+        name: 'tablePrefix',
+        message: '请输入表前缀：',
+      },
