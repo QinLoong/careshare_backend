fd15d3e642a1dd256ec30e2394a981beb4ea099a
QinLoong <2722937531@qq.com>
2023-06-26 20:37:27 +0800

    输入表名

--- plop-crud/index.js
+++ plop-crud/index.js
@@ -14,3 +14,10 @@ module.exports = {
         name: 'tablePrefix',
         message: '请输入表前缀：',
       },
+      {
+        type: 'input',
+        name: 'tables',
+        message: '请输入表名（多表以逗号隔开）：',
+        validate: (v) => (!v || v.trim() === '' ? '表名不能为空' : true),
+      },
+    ],
