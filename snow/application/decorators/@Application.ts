89051f2fe5bf254d06130c0718b0a00040333a4b
QinLoong <2722937531@qq.com>
2023-08-28 21:18:32 +0800

    import Logger from '../../logger';

--- snow/application/decorators/@Application.ts
+++ snow/application/decorators/@Application.ts
@@ -3,3 +3,5 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import { useController } from '../../controller';
+import Logger from '../../logger';
+import Logger from '../../logger';
