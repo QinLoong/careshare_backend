33f475cb8e740984d9f51d9ae2914b42f8f5767b
QinLoong <2722937531@qq.com>
2023-09-21 21:33:17 +0800

    export

--- snow/configuration/index.ts
+++ snow/configuration/index.ts
@@ -6,3 +6,11 @@ export interface ConfigOptions {
   rootPath?: string;
   database?: DatabaseOptions;
 }
+
+export interface DatabaseOptions {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+}
