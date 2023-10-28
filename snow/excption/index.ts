819b8bd2fc633adb4f017c9c92e0cb1316b79122
QinLoong <2722937531@qq.com>
2023-10-28 21:54:55 +0800

    数据库异常：适用于数据库操作发生的异常

--- snow/excption/index.ts
+++ snow/excption/index.ts
@@ -10,3 +10,16 @@ export class ParamsError extends Error {
     this.message = message || '参数异常';
   }
 }
+
+/**
+ * 数据库异常：适用于数据库操作发生的异常
+ */
+export class DatabaseError extends Error {
+  code: number
+  constructor(message?: string) {
+    super();
+    this.code = 403;
+    this.name = 'DatabaseError';
+    this.message = message || '数据库异常';
+  }
+}
