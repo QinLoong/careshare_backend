bcd25bbde4711a8833af7e4957cf22df0ed8f94d
QinLoong <2722937531@qq.com>
2023-11-07 09:11:56 +0800

    class Logger

--- snow/logger/index.ts
+++ snow/logger/index.ts
@@ -1,3 +1,8 @@
 import wistonLogger from './wistonLogger';
 
 type LogLevel = 'info' | 'warn' | 'error';
+
+class Logger {
+  static runtimeLog(level: LogLevel, message: string) {
+    wistonLogger.log(level, message);
+  }
