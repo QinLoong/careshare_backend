19b2452e7a5da5af8b9f3e84751e6ac20165108d
QinLoong <2722937531@qq.com>
2023-11-14 09:15:22 +0800

    const wistonLogger

--- snow/logger/wistonLogger.ts
+++ snow/logger/wistonLogger.ts
@@ -1,2 +1,9 @@
 import { createLogger, format, Logger, transports } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
+
+const wistonLogger: Logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(({ level, message, timestamp }) => `${timestamp} [${level}] ${message}`),
+    format.colorize({
