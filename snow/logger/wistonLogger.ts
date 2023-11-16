3d437909509cf5fb1ec08b6fece845a31a2ee291
QinLoong <2722937531@qq.com>
2023-11-16 09:16:16 +0800

    transports:

--- snow/logger/wistonLogger.ts
+++ snow/logger/wistonLogger.ts
@@ -16,3 +16,14 @@ const wistonLogger: Logger = createLogger({
       },
     }),
   ),
+  transports: [
+    // new transports.File({ filename: './log/runtime_error.log', level: 'error' }),
+    // new DailyRotateFile({
+    //   filename: './log/runtime_%DATE%.log',
+    //   datePattern: 'YYYYMMDD',
+    //   zippedArchive: false,
+    //   maxSize: '5g',
+    //   maxFiles: '14d',
+    // }),
+  ],
+});
