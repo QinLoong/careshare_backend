ebba8358452338ff8d6da6729c0e134155e07b19
QinLoong <2722937531@qq.com>
2023-11-26 09:24:07 +0800

    export function MqttGateway

--- snow/mqtt/decorators/@MqttGateway.ts
+++ snow/mqtt/decorators/@MqttGateway.ts
@@ -3,3 +3,7 @@
 import { IClientOptions } from 'mqtt';
 import 'reflect-metadata';
 import { META_KEYS } from './constants';
+
+export function MqttGateway(host: string, port: number, opts?: IClientOptions): ClassDecorator {
+  if (!host) throw new Error('mqtt的host不能为空');
+  if (!port) throw new Error('mqtt的port不能为空');
