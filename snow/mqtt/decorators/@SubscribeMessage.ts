dabed1f05b146cfa21fafb36094d56e910307a1b
QinLoong <2722937531@qq.com>
2023-12-02 09:27:11 +0800

    订阅的topicName不能为空

--- snow/mqtt/decorators/@SubscribeMessage.ts
+++ snow/mqtt/decorators/@SubscribeMessage.ts
@@ -1,3 +1,6 @@
 import 'reflect-metadata';
 import { META_KEYS } from './constants';
 import { TopicsMap } from './types';
+
+export function SubscribeMessage(topicName: string): MethodDecorator {
+  if (!topicName) throw new Error('订阅的topicName不能为空');
