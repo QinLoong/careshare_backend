86a2f248d6379313db6db2dbc4bc069dbcbccece
QinLoong <2722937531@qq.com>
2023-12-07 09:29:11 +0800

    eflect.hasMetadata(META_KEYS.CLIENT, instance, methodName

--- snow/mqtt/decorators/injector.ts
+++ snow/mqtt/decorators/injector.ts
@@ -7,3 +7,8 @@ export function getInjections(instance: any, methodName: string, message: string
     const index = Reflect.getMetadata(META_KEYS.MASSAGE_DATA, instance, methodName);
     props[index] = message;
   }
+
+  if (Reflect.hasMetadata(META_KEYS.CLIENT, instance, methodName)) {
+    const index = Reflect.getMetadata(META_KEYS.CLIENT, instance, methodName);
+    props[index] = client;
+  }
